2762e0e8746d4ce49d8ebf64ad5e0ae9dcc9005a
Hà Bùi Đức <habuiduc@Has-MacBook-Air.local>
2023-11-01 09:36:38 +0700

    connect firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -68,5 +69,9 @@ dependencies {
     implementation ("androidx.compose.material3:material3:1.1.2")
     implementation ("androidx.compose.material3:material3-window-size-class:1.1.2")
     implementation ("androidx.compose.material:material-icons-extended:1.5.4")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
 }
