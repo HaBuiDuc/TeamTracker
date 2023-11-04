b057e0770c73e43e7aa32d6a3aa7ea6397d2915d
admin <cong171002@gmail.com>
2023-11-04 10:06:37 +0700

    add TeamMemberDetail screen

--- app/src/main/java/com/buiducha/teamtracker/ui/screens/detail_project_screen/team_member_detail/TeamMemberDetail.kt
+++ app/src/main/java/com/buiducha/teamtracker/ui/screens/detail_project_screen/team_member_detail/TeamMemberDetail.kt
@@ -1,15 +1,20 @@
 package com.buiducha.teamtracker.ui.screens.detail_project_screen.team_member_detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -19,17 +24,25 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.buiducha.teamtracker.ui.states.fontWidth
+import com.buiducha.teamtracker.ui.screens.detail_project_screen.message_screen.demo_data.messages
 
 @Preview(showSystemUi = true)
 @Composable
 fun TeamMemberDetail(){
     var emailInput = remember {
+        mutableStateOf("")
+    }
+    var accountName = remember {
+        mutableStateOf("")
+    }
+    var phoneNumber = remember {
         mutableStateOf("")
     }
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
@@ -40,13 +53,14 @@ fun TeamMemberDetail(){
                 fontWeight = FontWeight.Bold,
             ),
             modifier = Modifier)
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text(text = "Nhập email vào ô dưới")
         Row {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 OutlinedTextField(
                     value = emailInput.value,
                     onValueChange = {},
-                    label = { Text(text = "Email Address") },
-                    placeholder = { Text(text = "Email Address") },
+                    placeholder = { Text(text = "Email") },
                     leadingIcon = {
                         Icon(
                             imageVector = Icons.Filled.MailOutline,
@@ -58,13 +72,31 @@ fun TeamMemberDetail(){
                 )
             }
         }
+        Spacer(modifier = Modifier.padding(10.dp))
         Row {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 OutlinedTextField(
-                    value = emailInput.value,
+                    value = accountName.value,
                     onValueChange = {},
-                    label = { Text(text = "Email Address") },
-                    placeholder = { Text(text = "Email Address") },
+                    placeholder = { Text(text = "Tên tài khoản") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.MailOutline,
+                            contentDescription = ""
+                        )
+                    },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(0.8f),
+                )
+            }
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                OutlinedTextField(
+                    value = phoneNumber.value,
+                    onValueChange = {},
+                    placeholder = { Text(text = "Số điện thoại") },
                     leadingIcon = {
                         Icon(
                             imageVector = Icons.Filled.MailOutline,
@@ -77,5 +109,20 @@ fun TeamMemberDetail(){
             }
         }
 
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "+ Thêm")
+        }
+        Spacer(modifier = Modifier
+            .padding(top = 10.dp)
+            .fillMaxWidth()
+            .height(2.dp)
+            .background(Color.Black))
+
+        Text(text = "Danh sách thành viên dự án")
+        LazyColumn(){
+            items(5){
+                TeamMemberList()
+            }
+        }
     }
 }
