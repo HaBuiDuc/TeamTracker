5d76567b416422e7633649987f46db5f4a1995c8
anh3112003qui <anh.qui0311@gmail.com>
2023-11-25 08:48:50 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
