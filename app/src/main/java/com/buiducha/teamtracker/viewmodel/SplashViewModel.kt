526b340aa9be727b36f129b597b8b1d0440e83ff
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-01-03 09:26:28 +0700

    check login and nav(login screen/home screen)

---
+++ app/src/main/java/com/buiducha/teamtracker/viewmodel/SplashViewModel.kt
@@ -0,0 +1,42 @@
+package com.buiducha.teamtracker.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.buiducha.teamtracker.repository.FirebaseRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashViewModel: ViewModel() {
+    private val fireBaseRepository = FirebaseRepository.get()
+
+    init {
+        viewModelScope.launch {
+        }
+    }
+
+    fun onLoginSuccess(
+        onUserExists: () -> Unit,
+        onUserNotExists: () -> Unit
+    ) {
+        fireBaseRepository.isUserInfoExists(
+            userId = fireBaseRepository.getCurrentUser()?.uid!!,
+            onUserExists = onUserExists,
+            onUserNotExists = onUserNotExists
+        )
+    }
+
+    fun checkAuthState(
+        onLogged: () -> Unit,
+        onNotLogged: () -> Unit
+    ) {
+        viewModelScope.launch {
+            delay(2000)
+            if (fireBaseRepository.getCurrentUser() != null) {
+                onLogged()
+            } else {
+                onNotLogged()
+            }
+        }
+
+    }
+}
